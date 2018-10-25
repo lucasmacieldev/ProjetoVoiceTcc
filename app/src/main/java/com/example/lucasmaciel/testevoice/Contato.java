0f3b24eef6ee493f53430c96cbbf7eed51c2780f
tearsz2 <42306362+tearsz2@users.noreply.github.com>
2018-10-24 21:46:19 -0300

    calculadora

--- app/src/main/java/com/example/lucasmaciel/testevoice/Contato.java
+++ app/src/main/java/com/example/lucasmaciel/testevoice/Contato.java
@@ -1,5 +1,6 @@
 package com.example.lucasmaciel.testevoice;
 
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,6 +13,19 @@ public class Contato extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate (savedInstanceState);
         setContentView (R.layout.activity_contato);
+        View decorView = getWindow().getDecorView();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_IMMERSIVE
+                            // Set the content to appear under the system bars so that the
+                            // content doesn't resize when the system bars hide and show.
+                            | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            // Hide the nav bar and status bar
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN);
+        }
 
         Button view = (Button)findViewById(R.id.viewButton);
         Button add = (Button)findViewById(R.id.createButton);
